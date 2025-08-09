d8a3bf332c1ec64ce0d55c444eb7e2b3e530a8da
Ruri-Kinoshita <b1023081@fun.ac.jp>
2025-08-09 20:37:31 +0900

    URLからアプリはひらけている

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:helloworld/routes.dart';
+import 'package:app_links/app_links.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,25 +10,51 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription<Uri>? _sub;
+  String? catchLink;
+  String? parameter;
+
+  @override
+  void initState() {
+    super.initState();
+    _sub = _appLinks.uriLinkStream.listen((Uri? uri) {
+      if (uri == null) return;
+      catchLink = uri.toString();
+      parameter = uri.queryParameters['name'];
+      setState(() {});
+    }, onError: (err) {
+      print('Error receiving link: $err');
+    });
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-        routerDelegate: goRouter.routerDelegate,
-        routeInformationParser: goRouter.routeInformationParser,
-        routeInformationProvider: goRouter.routeInformationProvider,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false
-        //home: const Startpage(),
-        );
+      routerDelegate: goRouter.routerDelegate,
+      routeInformationParser: goRouter.routeInformationParser,
+      routeInformationProvider: goRouter.routeInformationProvider,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
